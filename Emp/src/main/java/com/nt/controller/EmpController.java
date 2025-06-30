919fb913824b9435a6da8149d04c38f2a8cf5c12
ravikumar <ravikumark1150@gmail.com>
2025-06-30 06:33:22 +0530

    emp controller change

--- Emp/src/main/java/com/nt/controller/EmpController.java
+++ Emp/src/main/java/com/nt/controller/EmpController.java
@@ -1,63 +1,67 @@
 package com.nt.controller;
 
 import com.nt.entity.EmpDto;
-import com.nt.entity.Employee;
 import com.nt.sevice.EmpService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestController
 public class EmpController {
 
-    @Autowired
-    private EmpService empService;
+
+    private final EmpService empService;
+
+    public EmpController(EmpService empService) {
+        this.empService = empService;
+    }
 
 
     @GetMapping("/all")
-    public ResponseEntity<?> findAllEmp(){
+    public ResponseEntity<List<EmpDto>> findAllEmp() {
 
         List<EmpDto> emp = empService.findAllEmployee();
-
-        System.out.println(emp);
-
-      //  List<Employee> collect = emp.stream().collect(Collectors.toList());
 
         return new ResponseEntity<>(emp, HttpStatus.OK);
     }
 
-    @GetMapping("/byname")
-    public ResponseEntity<?> findEmpName(@RequestParam  String ename){
-
+    @GetMapping("/findbyname")
+    public ResponseEntity<List<EmpDto>> findEmpName(@RequestParam String ename) {
 
         List<EmpDto> employeeByName = empService.getEmployeeByName(ename);
-
-         return  new ResponseEntity<>(employeeByName, HttpStatus.OK);
+        return new ResponseEntity<>(employeeByName, HttpStatus.OK);
 
     }
 
-    @GetMapping("/seconSal/{deptNo}")
-    public ResponseEntity<EmpDto> findScondHighSal(@PathVariable String deptNo){
+    @GetMapping("/secondSal/{deptNo}")
+    public ResponseEntity<EmpDto> findScondHighSal(@PathVariable String deptNo) {
 
         Optional<EmpDto> secondHighSal = empService.findSecondHighSal(deptNo);
 
-        return ResponseEntity.ok().body(secondHighSal.get());
+        return secondHighSal.map(empDto -> new ResponseEntity<>(empDto, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+
 
     }
 
     @PatchMapping("/salUpdateByName/{salary}/{eName}")
-    public ResponseEntity<?> updateEmpSalary(@PathVariable Double salary,
-                                             @PathVariable String eName){
+    public ResponseEntity<String> updateEmpSalary(@PathVariable Double salary,
+                                                  @PathVariable String eName) {
 
         String s = empService.updateEmpSal(salary, eName);
 
-        return new  ResponseEntity<>(s,HttpStatus.OK);
+        return new ResponseEntity<>(s, HttpStatus.OK);
 
+
+    }
+
+    @GetMapping("/locbyname")
+    public ResponseEntity<Optional<String>> finEmpLocByName(@RequestParam String eName) {
+
+        Optional<String> empLocByName = empService.findEmpLocByName(eName);
+        return ResponseEntity.ok().body(empLocByName);
 
     }
 
